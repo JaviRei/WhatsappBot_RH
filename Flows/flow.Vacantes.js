5422de887d17f6cdaef65efbe1c987ca6dd40bf0
javier <javier.enriquez@shadesdemexico.com>
2024-02-17 12:37:02 -0600

    a

--- Flows/flow.Vacantes.js
+++ Flows/flow.Vacantes.js
@@ -1,3 +1,26 @@
 const { addKeyword } = require("@bot-whatsapp/bot");
+const getData = require("../formulas/get.Data");
 
-module.exports = addKeyword("1").addAnswer("Entraste al formulario");
+var data = getData();
+var res = [];
+
+for (key in data) {
+  try {
+    if (data[key].status == true) {
+      console.log("ID Activa: ", res);
+      return (res = data[key].id);
+    }
+  } catch {
+    console.error("Solicitud denegada");
+  }
+}
+
+const flowVacantes = addKeyword(res).addAnswer(
+  "Entraste al formulario",
+  null,
+  async () => {
+    console.log(res);
+  }
+);
+
+module.exports = { flowVacantes };
